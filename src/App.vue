03f73c7a28493cc66cca4d7b163354c9d4b9685b
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-21 16:34:08 +0100

    feat: Created a Vue chat app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    
   </nav>
   <router-view/>
+
 </template>
 
+<script setup>
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
