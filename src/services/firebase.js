a72323b6845e76bb2a77d70b487667f4504f7c93
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-21 16:36:54 +0100

    feat: installed and imported firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_F1X2K1fquVnkoixDBTKPn4Ql7xE9ZQw",
+  authDomain: "chat-cf2a6.firebaseapp.com",
+  databaseURL: "https://chat-cf2a6-default-rtdb.firebaseio.com",
+  projectId: "chat-cf2a6",
+  storageBucket: "chat-cf2a6.appspot.com",
+  messagingSenderId: "780078196383",
+  appId: "1:780078196383:web:084b3b9f6e7e12d722a4aa"
+};
+
+const fb = firebase.initializeApp(firebaseConfig);
+fb.firestore().settings({ timeStampsInSnapshots: true})
+
+export default fb.firestore();
