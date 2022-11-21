8d259f87662922888acb991f20cd4b4ca4171595
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-21 16:38:26 +0100

    feat: created chat page

---
+++ src/views/Chat.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="chat">
+        <h2>Chat</h2>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <p v-if="messages.length === 0">No messages yet</p>
+            <div class="messages">
+                <div v-for="message in messages" :key="message.id">
+                    <span>{{ message.name }}</span>
+                    <span>{{ message.message }}</span>
+                    <span>{{ message.timeStamp }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="card-action">
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const messages = ref([
+
+])
+</script>
