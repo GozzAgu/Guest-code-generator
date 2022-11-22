8dd7a434b1b506abba5e9dc0bf972a64f0d9b8d2
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-22 15:45:16 +0100

    fix: changed chat layout

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,28 +1,40 @@
 <template>
-    <div class="chat">
-        <h2>Chat</h2>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <p v-if="messages.length === 0">No messages yet</p>
-            <div class="messages">
-                <div v-for="message in messages" :key="message.id">
-                    <span>{{ message.name }}</span>
-                    <span>{{ message.message }}</span>
-                    <span>{{ message.timeStamp }}</span>
+    <div class="container">
+        <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 380px;">
+            <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
+                <h1 class="fs-5 fw-semibold">{{ username }}</h1>
+            </div>
+            <div class="list-group list-group-flush border-bottom scrollarea">
+                <div v-for="message in messages" :key="message" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                    <div class="d-flex w-100 align-items-center justify-content-between">
+                    <strong class="mb-1">{{ username }}</strong>
+                    <small class="text-muted">Wed</small>
+                    </div>
+                    <div class="col-10 mb-1 small text-start">{{ message }}</div>
                 </div>
             </div>
         </div>
-        <div class="card-action">
-            
-        </div>
+        <form @submit.prevent="submit">
+            <input v-model="message" class="form-control" placeholder="Write a message"/>
+        </form>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const messages = ref([
+const username = ref('Goz');
+const messages = ref([]);
+const message = ref('');
 
-])
+const submit = () => {
+    messages.value.push(message.value);
+    message.value = ''
+}
 </script>
+
+<style scoped>
+.scrollarea {
+    min-height: 500px;
+}
+</style>
