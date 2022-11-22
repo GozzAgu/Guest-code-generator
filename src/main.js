0e265a5ec775ba5a2b4995894319aa6a3002647f
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-22 15:44:46 +0100

    feat: Installed bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { createApp, } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp, } from 'vue';
+import App from './App.vue';
+import router from './router';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+// import Vue from 'vue';
+// import VueChatScroll from 'vue-chat-scroll';
 
 createApp(App)
 .use(router)
 .mount('#app')
 
+// Vue.use(VueChatScroll);
