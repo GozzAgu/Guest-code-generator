044316ccc2fb9f7cd20aa5c2e9777964661e293a
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-24 15:41:53 +0100

    fix: made message and array of objects

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="container">
+        <button @click="handlesignOut" v-if="isLoggedIn">Sign Out</button>
         <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 380px;">
             <div class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
                 <h1 class="fs-5 fw-semibold">{{ store.name }}</h1>
             </div>
             <div class="list-group list-group-flush border-bottom scrollarea">
-                <div v-for="message in messages" :key="message" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+                <div v-for="message, index in messages" :key="index" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
                     <div class="d-flex w-100 align-items-center justify-content-between">
-                        <strong class="mb-1">{{ message }}</strong>
-                        <small class="text-muted">{{ getTime().value }}</small>
+                        <strong class="mb-1">{{ message.text }}</strong>
+                        <small class="text-muted">{{ message.time }}</small>
                     </div>
-                    <div class="col-10 mb-1 small text-start">{{ message}}</div>
+                    <div class="col-10 mb-1 small text-start">{{ message.text }}</div>
                 </div>
             </div>
             <form @submit.prevent="sendMsg">
@@ -21,14 +22,29 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue'
+import { ref, defineProps, onMounted } from 'vue'
 import { useStore } from '@/store/store';
+import { useRouter } from 'vue-router';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
 
-const store = useStore()
+const isLoggedIn = ref(false);
+const store = useStore();
+const router = useRouter();
 const props = defineProps(['name'])
 const messages = ref([]);
 const message = ref('');
 
+let auth;
+onMounted(() => {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if(user) {
+            isLoggedIn.value = true;
+        } else {
+            isLoggedIn.value = false;
+        }
+    })
+})
 const getTime = () => {
     const date =  ref(new Date());
     const time = ref(date.value.getHours() + ':' + date.value.getMinutes() + ':' + date.value.getSeconds());
@@ -36,11 +52,20 @@ const getTime = () => {
 };
 
 const sendMsg = () => {
-    messages.value.push(message.value);
-    getTime();
+    messages.value.push({ 
+        text: message.value, 
+        time: getTime()
+    });
     message.value = '';
     props.name
 };
+
+const handlesignOut = () => {
+    signOut(auth)
+    .then(() => {
+        router.push('/');
+    })
+}
 
 </script>
 
