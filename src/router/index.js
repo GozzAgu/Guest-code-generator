38c115b6147fdef858e173256f6bac2abdd6cab6
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-24 15:40:52 +0100

    feat: added sign in route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// import { getAuth, onAuthStateChanged } from 'firebase/auth'
 // import HomeView from '../views/HomeView.vue'
 import Signup from '../views/SignUp.vue'
+import Signin from '../views/SignIn.vue'
 import Chat from '../views/Chat.vue'
 
 const routes = [
@@ -13,19 +15,48 @@ const routes = [
   {
     path: '/',
     name: 'signup',
-    component: Signup
+    component: Signup,
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: Signin
   },
   {
     path: '/chat',
     name: 'chat',
     component: Chat,
     props: true,
+    requiresAuth: true
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
+
+// const getCurrentuser = () => {
+//   return new Promise((resolve, reject) => {
+//     const removeListener = onAuthStateChanged(
+//       getAuth(),
+//       (user) => {
+//         removeListener();
+//         resolve(user);
+//       },
+//       reject
+//     );
+//   });
+// };
+
+// router.beforeEach(async(to, from, next) => {
+//   if(to.matched.some((record) => record.meta.requiresAuth)) {
+//     if(await getCurrentuser) {
+//       next();
+//     }
+//   } else {
+//     next('/');
+//   }
+// })
 
 export default router
