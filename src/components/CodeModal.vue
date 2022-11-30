9a84c1c5928b981df572b8f5ced24ae7b56e48dc
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-30 11:13:41 +0100

    feat: added codeModal component

---
+++ src/components/CodeModal.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <textarea v-model="code">
+            
+        </textarea>
+        <button @click="getCode">Generate</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const code = ref('')
+
+const getCode = () => {
+    code.value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+}
+</script>
