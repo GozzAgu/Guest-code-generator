d3f9fee70ac072e0402c44e68a02376b75ab30e9
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-30 12:12:34 +0100

    feat: styled the buttons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,20 +11,22 @@
                         </router-link>
                     </blockquote>
                 </figure>
-                <button  v-if="isLoggedIn" @click="handlesignOut" type="button" class="btn btn-secondary text-light">Sign Out</button>
+                <button  v-if="isLoggedIn" @click="handlesignOut" class="btn btn-secondary text-light col-3 ms-auto">Sign Out</button>
                 <router-link to="/signin" v-else>
-                    <button type="button" class="col-4 btn btn-secondary text-light">Sign In</button>
+                    <button class="col-4 btn btn-secondary text-light col-3 ms-auto">Sign In</button>
                 </router-link>
             </div>
         </div>
         <div class="d-flex">
-            <button @click="showModal = true" class="card m-2 col-3 col-lg-2">
-                <p>Gate Pass</p>
+            <button @click="showModal = true" class="btn btn-light m-2 col-3 col-lg-2">
+                Gate Pass
             </button>
         </div>
         <CodeModal v-if="showModal"/>
 
-        <div class="mt-5">
+        <h3 class="mt-5 text-start"><i class="ri-draft-line"></i> Guest Information </h3>
+
+        <div class="mt-1">
             <table class="table table-striped table-hover">
             <thead class="bg-secondary text-light">
                 <tr>
