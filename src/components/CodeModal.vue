f1411ba106bfb6a2ab528b78190c446dc3bc0946
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-11-30 12:11:47 +0100

    fix: refactored the generate code modal

--- src/components/CodeModal.vue
+++ src/components/CodeModal.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="card">
-        <textarea v-model="code">
-            
-        </textarea>
-        <button @click="getCode">Generate</button>
-    </div>
+    <div class="card p-3 col-8 col-lg-4 ms-auto me-auto">
+        <h5>Generate your code</h5>
+        <input placeholder="Guest's name" v-model="name"/>
+        <h3>code: {{ code }}</h3>
+        <button class="btn btn-light col-3 col-lg-2 ms-auto me-auto" @click="getCode"><i class="ri-settings-4-fill"></i></button>
+    </div> 
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const code = ref('')
+const code = ref('');
+
+const name = ref('');
 
 const getCode = () => {
     code.value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
