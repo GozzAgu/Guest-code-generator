367b36e4f91992570341a33fb39a4ece95573db1
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2022-12-02 12:02:17 +0100

    feat: added number of guests

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,20 +17,26 @@
                 </router-link>
             </div>
         </div>
+        
         <div class="d-flex">
             <button @click="showModal = true" class=" shadow-sm btn btn-light m-2 col-3 col-lg-2">
-                Gate Pass
+                + New Guest
             </button>
         </div>
+
+        <div class="mt-3 border-bottom">
+            <h3>You've had {{ visitors.length }} guest(s) so far!</h3>
+        </div>
+
         <CodeModal @close="showModal = false" v-if="showModal" @onSubmit="newGuest"/>
 
         <h3 class="mt-5 text-start"><i class="ri-draft-line"></i> Guest Log </h3>
 
-        <div class="mt-1">
+        <div class="mt-1 shadow">
             <table class="table table-striped table-hover">
                 <thead class="bg-secondary text-light">
                     <tr>
-                    <th scope="col">Status</th>
+                    <!-- <th scope="col">Status</th> -->
                     <th scope="col">Visitor's name</th>
                     <th scope="col">Code</th>
                     <th scope="col">Gender</th>
@@ -39,7 +45,7 @@
                 </thead>
                 <tbody v-for="visitor in visitors" :key="visitor">
                     <tr class="mb-5">
-                        <td>{{ visitor.status }}</td>
+                        <!-- <td>{{ visitor.status }}</td> -->
                         <td>{{ visitor.name }}</td>
                         <td>{{ visitor.code }}</td>
                         <td>{{ visitor.gender }}</td>
